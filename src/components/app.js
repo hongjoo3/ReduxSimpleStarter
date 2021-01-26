dacc77cec59300c5e4f9dc206175a1a403dec34a
hongjookim <hongjoo@companylabs.co.kr>
2021-01-26 17:28:39 +0900

    #1.0 reactredux udemy course

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,43 @@
+import _ from  'lodash';
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from "./search_bar";
+import VideoList from "./video_list";
+import VideoDetail from './video_detail';
+
+const API_KEY = "AIzaSyB_2NJ8cd16p2m4Lxlj2Y-QliR6VyQktsE";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term},  (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => videoSearch(term)}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos}/>
+            </div>
+        );
+    }
 }
